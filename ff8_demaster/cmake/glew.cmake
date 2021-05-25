7be0acf5e257d93adf24c82787a004079667e5a8
Sebanisu <peeceseven@gmail.com>
2021-05-25 13:31:50 -0400

    Linked to the glew via cmake and github.
    This means this is dynamicly fetched when you run cmake. Though this will break the visual studio project as the files won't be there now.

--- ff8_demaster/cmake/glew.cmake
+++ ff8_demaster/cmake/glew.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 #options for FetchContent are at https://cmake.org/cmake/help/latest/module/ExternalProject.html
 FetchContent_Declare(
-        glew
+        glew_fetch
         #GIT_REPOSITORY https://github.com/nigels-com/glew.git # do not reference this directly requires generating source fork does that for us.
         #GIT_TAG origin/master
         GIT_REPOSITORY https://github.com/Perlmint/glew-cmake.git
@@ -9,8 +9,8 @@ FetchContent_Declare(
         GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
 )
 #FetchContent_MakeAvailable(glew)
-FetchContent_GetProperties(glew)
-if (NOT glew_POPULATED)
-    FetchContent_Populate(glew)
-    add_subdirectory(${glew_SOURCE_DIR} ${glew_BINARY_DIR} EXCLUDE_FROM_ALL)
+FetchContent_GetProperties(glew_fetch)
+if (NOT glew_fetch_POPULATED)
+    FetchContent_Populate(glew_fetch)
+    add_subdirectory(${glew_fetch_SOURCE_DIR} ${glew_fetch_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif ()
