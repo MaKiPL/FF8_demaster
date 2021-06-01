47641b5da5ab305b80c1e4a33719839f11f3163a
Sebanisu <peeceseven@gmail.com>
2021-06-01 08:36:33 -0400

    Update texturepatch_v2_world.cpp
    I added some code to see about the textureType == 0
    so as it had extra texture pages. So I wanted to check if they were different or not.
    most of them are probably a variant of the minimap. I decided to leave code there encase we wanted to look closer at it later.
    
    I disabled the noticeable clouds and minimap colliding. Hopefully when you get time to debug this you can figure out a better way to handle them.
    right now you can't change one with out the other changing.

--- ff8_demaster/texturepatch_v2_world.cpp
+++ ff8_demaster/texturepatch_v2_world.cpp
@@ -45,15 +45,22 @@ struct worldTextureStructure ws[] =
 	{"", 23, NULL, -1,-1,-1,FALSE}, //MAP
 	{"", 24, NULL, -1,-1,-1,FALSE}, //moon texture + fx
 	{"", 25, NULL, -1,-1,-1,FALSE}, //WATER
-	{"", 26, NULL, -1,-1,-1,FALSE}, //15
-	{"", 27, NULL, -1,-1,-1,FALSE}, //16
+	{"", 26, NULL, -1,-1,-1,FALSE}, //characters
+	{"", 27, NULL, -1,-1,-1,FALSE}, //16 ??? textype=18
+	{"", 28, NULL, -1,-1,-1,FALSE},	// 0: textype=0
+	{"", 29, NULL, -1,-1,-1,FALSE},	// 13: textype=0 full screen wm, sm wm, clouds
+	{"", 30, NULL, -1,-1,-1,FALSE},	// 15: textype=0 sm wm
+	{"", 31, NULL, -1,-1,-1,FALSE},	// 20: textype=0
+	{"", 32, NULL, -1,-1,-1,FALSE},	// 22: textype=0 sm wm
+	{"", 33, NULL, -1,-1,-1,FALSE},	// 28: textype=0 sm wm
+	{"", 34, NULL, -1,-1,-1,FALSE},	// 29: textype=0
 };
 
 int GetTextureIndex();
 
 //you can't just create new folder because >WEEP< - too lazy to find the cause
 
-DWORD lastKnownTextureId{0};
+DWORD lastKnownTextureId{ 0 };
 void LoadImageIntoWorldStruct(size_t texIndex, const char* const localn, int tPage, int palette)
 {
 
@@ -80,6 +87,7 @@ void _wtpGl()
 {
 	DWORD tPage = gl_textures[50];
 	int palette = tex_header[52];
+	int textureType = gl_textures[48];
 	char localn[256]{ 0 };
 
 	DWORD unk = *(DWORD*)(IMAGE_BASE + 0x17424B4);
@@ -88,40 +96,48 @@ void _wtpGl()
 	int getTexIndex = GetTextureIndex();
 	if (getTexIndex <= 0) return;
 
-	if (texIndex != getTexIndex
-		/*getTexIndex == 13 ||
-		getTexIndex == 14 ||
-		getTexIndex == 15 ||
-		getTexIndex == 16 ||
-		getTexIndex == 24 ||
-		getTexIndex == 28 ||
-		getTexIndex == 29 ||
-		texIndex == 0*/
-		)
+	if (textureType == 18 && texIndex == getTexIndex)
+		//if (getTexIndex < 20 && (tPage > 14 && tPage < 26) )
 	{
 
-		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset_%03d_0", DIRECT_IO_EXPORT_DIR, getTexIndex))
-		{
-			OutputDebug("%s::%d::Failed to Load: %s\n\tgetTexIndex: %d\n", __func__, __LINE__, localn, getTexIndex);
-			return;
-		}
+		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\texl\\texl_%03d_%d", DIRECT_IO_EXPORT_DIR, /*tPage*/texIndex, palette))
+			if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\texl\\texl_%03d_%d", DIRECT_IO_EXPORT_DIR, /*tPage*/texIndex, 0))
+			{
+				OutputDebug("%s::%d::Failed to Load: %s\n\tgetTexIndex: %d, tPage: %d, texIndex: %d, palette: %d", __func__, __LINE__, localn, getTexIndex, tPage, texIndex, palette);
+				return;
+			}
 
+		LoadImageIntoWorldStruct(texIndex, localn, tPage, palette);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		RenderTexture(ws[texIndex].buffer);
+
+		return;
+	}
+	else if (textureType == 18)
+	{
+		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset_%03d_%d", DIRECT_IO_EXPORT_DIR, getTexIndex, palette))
+			if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset_%03d_%d", DIRECT_IO_EXPORT_DIR, getTexIndex, 0))
+			{
+				OutputDebug("%s::%d::Failed to Load: %s\n\tgetTexIndex: %d\n palette: %d", __func__, __LINE__, localn, getTexIndex, palette);
+				return;
+			}
 		int wmStructPointer = 21;
 		switch (getTexIndex)
 		{
 		case 13: //moon texture + fx
 			wmStructPointer = 24;
 			break;
-		case 14: //clouds
+		case 14: //clouds or atleast is used to be.
 			wmStructPointer = 22;
 			break;
-		case 15: //
+		case 15: //characters and vehicles
 			wmStructPointer = 26;
 			break;
-		case 16: //
+		case 16: //???
 			wmStructPointer = 27;
 			break;
-		case 24:
+		case 24: //???
 			wmStructPointer = 23;
 			break;
 		case 28: // water
@@ -131,25 +147,48 @@ void _wtpGl()
 			wmStructPointer = 21;
 			break;
 		}
-
 		LoadImageIntoWorldStruct(wmStructPointer, localn, tPage, palette);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		RenderTexture(ws[wmStructPointer].buffer);
 	}
-	else  //if (getTexIndex < 20 && (tPage > 14 && tPage < 26) )
+	else if (textureType == 0)
 	{
-
-		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\texl\\texl_%03d_0", DIRECT_IO_EXPORT_DIR, /*tPage*/texIndex))
+		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset0_%03d_%d", DIRECT_IO_EXPORT_DIR, getTexIndex, palette))
+			if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset0_%03d_%d", DIRECT_IO_EXPORT_DIR, getTexIndex, 0))
+			{
+				OutputDebug("%s::%d::Failed to Load: %s\n\tgetTexIndex: %d\n palette: %d", __func__, __LINE__, localn, getTexIndex, palette);
+				return;
+			}		
+		int wmStructPointer = 28;
+		switch (getTexIndex)
 		{
-			OutputDebug("%s::%d::Failed to Load: %s\n\tgetTexIndex: %d, tPage: %d, texIndex: %d", __func__, __LINE__, localn, getTexIndex, tPage, texIndex);
-			return;
+		case 0: //
+			wmStructPointer = 28;
+			break;
+		case 13: //full screen wm, sm wm, clouds
+			wmStructPointer = 28;
+			break;
+		case 15: //sm wm
+			wmStructPointer = 30;
+			break;
+		case 20: //
+			wmStructPointer = 31;
+			break;
+		case 22: //sm wm
+			wmStructPointer = 32;
+			break;
+		case 28: //sm wm
+			wmStructPointer = 33;
+			break;
+		case 29: //
+			wmStructPointer = 34;
+			break;
 		}
-
-		LoadImageIntoWorldStruct(texIndex, localn, tPage, palette);
+		LoadImageIntoWorldStruct(wmStructPointer, localn, tPage, palette);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		RenderTexture(ws[texIndex].buffer);
+		RenderTexture(ws[wmStructPointer].buffer);
 	}
 	return;
 }
@@ -183,7 +222,7 @@ int GetTextureIndex()
 			lastKnownTextureId = texD;
 			return texD;
 		case 13: //moon texture + fx
-		case 14: //clouds
+		case 14: //clouds or atleast used to be
 		case 28: //water textures
 		case 29: //tracks, bridge, vehicle
 		//case 16: //??? I see it trying to load this and don't see anything using it.
@@ -194,18 +233,29 @@ int GetTextureIndex()
 		}
 		//OutputDebug("%s::tPage: %d\n", __func__, tPage);
 	}
-	else if (textureType == 196)
-	{
-		//tPage 13, 15, 28
-	}
-	else if (textureType == 201)
-	{
-		//tPage 13, 15, 28
-	}
 	else if (textureType == 0) //map
 	{
-		//OutputDebug("%s::%d::tPage: %d, textureType: %d, palette: %d\n", __func__, __LINE__, tPage, textureType, palette);
-		return -1;
+		switch (tPage)
+		{
+		case 13: //full screen wm, sm wm, clouds
+		case 15: // sm wm
+		case 22: // sm wm
+		case 28: // sm wm
+			// when small map up 15,22,28 swaps / flickers with 13
+			//return 13;
+			return -1; //disabled to due to cloulds colliding with minimap
+		case 0: //??? probably more minimaps 
+		case 20: //???
+		case 29: //???
+			return tPage;
+		default:
+			OutputDebug("%s::%d::tPage: %d, textureType: %d, palette: %d\n", __func__, __LINE__, tPage, textureType, palette);
+			break;
+		}
+	}
+	else
+	{
+		OutputDebug("%s::%d::tPage: %d, textureType: %d, palette: %d\n", __func__, __LINE__, tPage, textureType, palette);
 		//return 24;
 	}
 	return -1;
@@ -219,23 +269,35 @@ DWORD _wtpCheck()
 		return 0;
 
 	int textureType = gl_textures[48];
-	if (textureType != 18 && textureType != 0) //we want only world textures
+		if (textureType != 18 && textureType != 0) //we want only world textures
 		return 0;
 
 	DWORD tPage = gl_textures[50];
+	int palette = tex_header[52];
 
 	char localn[256]{ 0 };
 	int textureIndex = GetTextureIndex();
 
-	if (textureIndex <= 0) //disabled textures are set to -1;
+	if (textureIndex < 0) //disabled textures are set to -1;
 		return 0;
-	if (lastKnownTextureId == textureIndex)//(textureIndex < 20 && (tPage > 14 && tPage < 26))
+	if (textureType == 18 && lastKnownTextureId == textureIndex)//(textureIndex < 20 && (tPage > 14 && tPage < 26))
 	{
-		DDSorPNG(localn, 256, "%stextures\\world\\dat\\texl\\texl_%03d_0", DIRECT_IO_EXPORT_DIR, textureIndex);
+		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\texl\\texl_%03d_%d", DIRECT_IO_EXPORT_DIR, textureIndex, palette))
+			DDSorPNG(localn, 256, "%stextures\\world\\dat\\texl\\texl_%03d_%d", DIRECT_IO_EXPORT_DIR, textureIndex, 0);
 	}
-	else
+	else if(textureType == 18)
 	{
-		DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset_%03d_0", DIRECT_IO_EXPORT_DIR, textureIndex);
+		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset_%03d_%d", DIRECT_IO_EXPORT_DIR, textureIndex, palette))
+			DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset_%03d_%d", DIRECT_IO_EXPORT_DIR, textureIndex, 0);
+	}
+	else if (textureType == 0)
+	{
+		//giving different name to avoid collisions as some of these have the same textureIndex.
+		// I do wonder if the texture index does anything for this part. Minimap seems to use many different ones.
+		if (DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset0_%03d_%d", DIRECT_IO_EXPORT_DIR, textureIndex, palette))
+			DDSorPNG(localn, 256, "%stextures\\world\\dat\\wmset\\wmset0_%03d_%d", DIRECT_IO_EXPORT_DIR, textureIndex, 0);
+
+		//return 1; //force attempt to override
 	}
 
 	if (GetFileAttributesA(localn) == INVALID_FILE_ATTRIBUTES)
