e63458838385f15271af1fc8a49e75f329be2a22
Maki <marcingomulak@gmail.com>
2024-08-01 09:44:33 +0200

    Server is now writing buffers in try/catch to prevent crash + it's automatically disabled after 5 tries

--- ff8_demaster/server.cpp
+++ ff8_demaster/server.cpp
@@ -25,17 +25,31 @@ server::~server()
 }
 #endif
 
-void server::WriteBuffer(const char* buffer, const size_t size) const
+void server::WriteBuffer(const char* buffer, const size_t size)
 {
 #if USE_SERVER
-    asio::io_context io_context;
-    asio::ip::tcp::resolver resolver(io_context);
-    asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(ip, std::to_string(port));
-    asio::ip::tcp::socket socket(io_context);
-    asio::connect(socket, endpoints);
+    if(ignoreServer)
+        return;
+    try
+    {
+        asio::io_context io_context;
+        asio::ip::tcp::resolver resolver(io_context);
+        asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(ip, std::to_string(port));
+        asio::ip::tcp::socket socket(io_context);
+        asio::connect(socket, endpoints);
     
-    asio::write(socket, asio::buffer(buffer, size));
-    socket.shutdown(asio::ip::tcp::socket::shutdown_send);
-    socket.close();
+        asio::write(socket, asio::buffer(buffer, size));
+        socket.shutdown(asio::ip::tcp::socket::shutdown_send);
+        socket.close();
+    }
+    catch (std::exception& e)
+    {
+        OutputDebug("Failed to write buffer to server: %s\n", e.what());
+        if(++retries>MAX_RETRIES)
+        {
+            ignoreServer = true;
+            OutputDebug("Server connection lost, disabling writes for this client.\n");
+        }
+    }
 #endif
 }
--- ff8_demaster/server.h
+++ ff8_demaster/server.h
@@ -7,9 +7,13 @@ class server
 public:
     explicit server(const std::string& ip = "127.0.0.1", int port=1337);
     ~server();
-    void WriteBuffer(const char* buffer, size_t size) const;
+    void WriteBuffer(const char* buffer, size_t size);
 
 private:
     std::string ip;
     int port;
+
+    const int MAX_RETRIES = 5;
+    int retries = 0;
+    bool ignoreServer = false;
 };
